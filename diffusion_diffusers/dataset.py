8ecaf921e8f9162757e90eb4222cbd44918d1e76
wangyanhui666 <wangyanhui@mail.ustc.edu.cn>
2024-09-03 12:10:51 +0000

    start add imagenet training script

---
+++ diffusion_diffusers/dataset.py
@@ -0,0 +1,26 @@
+import os
+
+import torch
+import numpy as np
+from torch.utils.data import Dataset
+
+class CustomDataset(Dataset):
+    def __init__(self, features_dir, labels_dir):
+        self.features_dir = features_dir
+        self.labels_dir = labels_dir
+
+        self.features_files = sorted(os.listdir(features_dir))
+        self.labels_files = sorted(os.listdir(labels_dir))
+
+    def __len__(self):
+        assert len(self.features_files) == len(self.labels_files), \
+            "Number of feature files and label files should be same"
+        return len(self.features_files)
+
+    def __getitem__(self, idx):
+        feature_file = self.features_files[idx]
+        label_file = self.labels_files[idx]
+
+        features = np.load(os.path.join(self.features_dir, feature_file))
+        labels = np.load(os.path.join(self.labels_dir, label_file))
+        return torch.from_numpy(features), torch.from_numpy(labels)
