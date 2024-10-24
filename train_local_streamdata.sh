da5fd0a1627dda3a22c1984730e2b7bc5eccabff
wangyanhui666 <wangyanhui@mail.ustc.edu.cn>
2024-10-24 14:22:52 +0000

    deleted files

--- train_local_streamdata.sh
+++ train_local_streamdata.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 export NCCL_SOCKET_IFNAME=eth0
-export WANDB_API_KEY="local-9fd3e86800565dfa4b2a5592ccfaf45ef59ec0c6"
+# export WANDB_API_KEY="your_wandb_api_key"
 # --gradient_accumulation_steps=4 \
 # --gradient_checkpointing \
-wandb login local-9fd3e86800565dfa4b2a5592ccfaf45ef59ec0c6 --relogin --host=https://microsoft-research.wandb.io
+# export WANDB_API_KEY="your_wandb_api_key"
+# # wandb login
+# wandb login
+
 accelerate launch \
-    --config_file /home/t2vg-a100-G4-42/code/DiT/config/default_config.yaml \
+    --config_file ./config/default_config.yaml \
     train_diffusers_imagenet_streamdata.py \
     --local-dataset-path ~/datasets/vae_mds_fp32 \
     --vae-path stabilityai/sd-vae-ft-mse \
@@ -15,8 +18,8 @@ accelerate launch \
     --dataloader_num_workers 1 \
     --num_epochs 1000 \
     --save_model_epochs 40000 \
-    --output_dir ~/guangtingsc/t2vg/dit/logs/train_imagenet_256/1018_imagenet_fp32_fix_flip_writedatabug_2 \
-    --logger wandb \
+    --output_dir ./debug_ckptdir \
+    --logger tensorboard \
     --mixed_precision bf16 \
     --checkpointing_steps 40000 \
     --resume_from_checkpoint latest \
